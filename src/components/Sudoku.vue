cbd8a98288cf4d946cf836712a94eee2aab4e775
cynthiameiring <cynthia.meiring@hotmail.com>
2020-02-15 11:37:56 +0100

    translate boardString to a grid with styling

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="sudoku">
     <h2>Sudoku</h2>
+    <div class="grid">
+      <div class="row" v-for="(row, rowIndex) in puzzle" v-bind:key="rowIndex">
+        <div
+          class="cell"
+          v-for="(cell, colIndex) in row"
+          :key="colIndex"
+          v-bind:class="{'border-right': colIndex===2||colIndex===5,'border-bottom': rowIndex===2||rowIndex===5}"
+        >{{ cell }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,10 +31,11 @@ export default {
   },
   methods: {
     generatePuzzle() {
+      //build-in method in the sudoku.js
       const boardString = sudoku.generate(this.difficulty);
       //build-in method in the sudoku.js
       this.puzzle = sudoku.board_string_to_grid(boardString);
-      console.log(this.puzzle);
+      console.log("puzzle:", this.puzzle);
     }
   }
 };
@@ -32,8 +43,33 @@ export default {
 
 <style scoped>
 .sudoku {
-  width: 400px;
+  width: calc(9 * 40px);
   margin: 0.5rem auto;
   font-family: Arial, Helvetica, sans-serif;
+}
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.cell {
+  display: block;
+  width: 40px;
+  height: 40px;
+  /* border size included in the widht: */
+  box-sizing: border-box;
+  border: 1px solid #bbb;
+  font-size: 24px;
+  line-height: 40px;
+  text-align: center;
+  cursor: default;
+}
+.cell.border-right {
+  border-right-width: 3px;
+  border-right-color: rgb(139, 139, 139);
+}
+.cell.border-bottom {
+  border-bottom-width: 3px;
+  border-bottom-color: rgb(139, 139, 139);
 }
 </style>
