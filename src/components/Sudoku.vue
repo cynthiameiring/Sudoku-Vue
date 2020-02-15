f3d0a5418ce707ddca4be5e5e34da2ebaf1bfd72
cynthiameiring <cynthia.meiring@hotmail.com>
2020-02-15 15:30:12 +0100

    check if value is invalid

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -12,6 +12,8 @@
             'border-right': colIndex===2||colIndex===5,
             'border-bottom': rowIndex===2||rowIndex===5, 
             'startValue':cell.startValue,
+            //if the cell has a value then check if it is valid:
+            'invalid': cell.value && isCellInvalid(rowIndex,colIndex,cell.value),
             'active':activeRow===rowIndex&&activeCol===colIndex }"
         >{{ cell.value }}</div>
       </div>
@@ -23,6 +25,7 @@
         v-for="value in Array(9).keys()"
         :key="value"
         :disabled="activeRow===null || activeCol===null"
+        @click="setCellValue(value+1)"
       >{{value+1}}</button>
     </div>
   </div>
@@ -74,65 +77,38 @@ export default {
       }
       this.activeRow = row;
       this.activeCol = col;
+    },
+    setCellValue(value) {
+      this.puzzle[this.activeRow][this.activeCol].value = value;
+      this.activeRow = null;
+      this.activeCol = null;
+    },
+    isCellInvalid(row, col, value) {
+      for (let c = 0; c < 9; c++) {
+        if (this.puzzle[row][c].value === value && c !== col) {
+          return true;
+        }
+      }
+      for (let r = 0; r < 9; r++) {
+        if (this.puzzle[r][col].value === value && r !== row) {
+          return true;
+        }
+      }
+      const rowStart = Math.floor(row / 3) * 3;
+      const colStart = Math.floor(col / 3) * 3;
+      for (let r = rowStart; r < rowStart + 3; r++) {
+        for (let c = colStart; c < colStart + 3; c++) {
+          if (this.puzzle[r][c].value === value && !(r === row && c === col)) {
+            return true;
+          }
+        }
+      }
+      return false;
     }
   }
 };
 </script>
 
-<style scoped>
-.sudoku {
-  width: 100%;
-  max-width: 420px;
-  margin: 0.5rem auto;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.grid {
-  width: calc(9 * 40px);
-  margin: 0.5rem auto 1rem;
-}
-.row {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-.cell {
-  display: block;
-  width: 40px;
-  height: 40px;
-  /* border size included in the widht: */
-  box-sizing: border-box;
-  border: 1px solid #bbb;
-  font-size: 24px;
-  line-height: 40px;
-  text-align: center;
-  cursor: default;
-}
-.cell.border-right {
-  border-right-width: 3px;
-  border-right-color: rgb(139, 139, 139);
-}
-.cell.border-bottom {
-  border-bottom-width: 3px;
-  border-bottom-color: rgb(139, 139, 139);
-}
-.cell.startValue {
-  font-weight: bold;
-}
-.cell:not(.startValue) {
-  cursor: pointer;
-}
-.cell.active {
-  background-color: blue;
-  color: white;
-}
-.btn {
-  width: 38px;
-  height: 38px;
-  font-size: 24px;
-  cursor: pointer;
-}
-.btn:disabled {
-  cursor: not-allowed;
-}
+<style>
+@import "../style.css";
 </style>
